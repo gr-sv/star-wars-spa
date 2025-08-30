eb11df6de97a876be3ca7429b0b3bb6654354e8f
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-08-30 16:11:51 +0300

    Add Card

---
+++ src/components/widgets/card/card.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./card.module.scss"
+
+function Card() {
+	return (
+		<div className={s.card}>		
+			<p>Character</p>
+		</div>
+	)
+}
+
+export default Card;
