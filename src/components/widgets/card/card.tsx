266472766e0604ef3e2d4f900b730bc1e1dfb1c5
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-03 22:57:32 +0300

    Add Card, styles

--- src/components/widgets/card/card.tsx
+++ src/components/widgets/card/card.tsx
@@ -4,7 +4,9 @@ import s from "./card.module.scss"
 function Card() {
 	return (
 		<div className={s.card}>		
-			<p>Character</p>
+			<div className={s.cardIconBack} />
+			<h2 className={s.cardName}>Name</h2>
+			<div className={s.cardUnderlining} />
 		</div>
 	)
 }
