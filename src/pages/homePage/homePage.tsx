334571b6d19b50b7e439873d5ad9b8fc5e097edb
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-08 15:52:31 +0300

    Get external data

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+'use client'
+
+import React, { useState, useEffect } from 'react';
 import Header from '../../widgets/header/header';
 import CharacterCards from '../../widgets/characterCards/characterCards';
+import axios from 'axios';
 
 function HomePage() {
+
+	const[data, setData] = useState([]);
+	const[loading, setLoading] = useState(false);
+
+	useEffect(() => {
+
+		const fetchData = async () => {
+			try{
+				setLoading(true);
+				const response =await axios.get('https://swapi.py4e.com/api/people/');
+
+				setLoading(false);
+				setData(response.data.results);
+
+			} catch (e) {
+				console.log('Error:', e);
+
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	console.log(data);
+
 	return (
 		<>
 			<Header />
