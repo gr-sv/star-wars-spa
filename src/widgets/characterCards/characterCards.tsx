7785ee9940bc3bbff5ea188c5b71d58188109bd1
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-08 21:50:43 +0300

    Add interfaces, redesign

--- src/widgets/characterCards/characterCards.tsx
+++ src/widgets/characterCards/characterCards.tsx
@@ -1,25 +1,28 @@
 import React from "react";
-import s from './chatacterCards.module.scss'
-import Wrapper from "../wrapper/wrapper";
-import Card from "../card/card";
+import s from './chatacterCards.module.scss';
+import Wrapper from '../wrapper/wrapper';
 
-function CharacterCards() {
+interface Character {
+    name: string;
+}
+
+interface CharacterCardsProps {
+	characters: Character[];
+};
+
+function CharacterCards({ characters }: CharacterCardsProps) {
 	return (
 		<Wrapper>
-			<div className={s.cards}>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />				
-			</div>
+			
+			{characters.map(character => (
+				<div className={s.cards}>
+					<div key={character.name} className={s.cardIconBack} />
+					<h2 className={s.cardName}>{character?.name}</h2>
+				</div>
+			))}
+			
 		</Wrapper>
 	)
-
 }
 
 export default CharacterCards;
