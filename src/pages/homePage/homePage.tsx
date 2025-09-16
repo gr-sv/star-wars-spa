3563ceccbaeb1da59afda32ff38e1c1094db3035
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-16 22:05:57 +0300

    Add icons into pagination, styles

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -5,6 +5,8 @@ import Header from '../../widgets/header/header';
 import CharacterCards from '../../widgets/characterCards/characterCards';
 import axios from 'axios';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import s from './homePage.module.scss';
 
 function HomePage() {
@@ -74,8 +76,11 @@ function HomePage() {
 				<CharacterCards characters={charactersOnPage} loading={loading}/>
 
 				<div className={s.pagination}>
-					<button className={s.paginationButtonPrev} onClick={prevPage}>
+					<button className={`${s.paginationLink} ${s.paginationButton}`} onClick={prevPage}>
 						<span className='visually-hidden'>Prev Page</span>
+						<svg className={s.paginationIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+							<path d="M167.5 303C158.1 312.4 158.1 327.6 167.5 336.9L367.5 536.9C376.9 546.3 392.1 546.3 401.4 536.9C410.7 527.5 410.8 512.3 401.4 503L218.4 320L401.4 137C410.8 127.6 410.8 112.4 401.4 103.1C392 93.8 376.8 93.7 367.5 103.1L167.5 303z" />
+						</svg>
 					</button>
 
 					{pageNumbers.map(pageNumber => (
@@ -84,8 +89,11 @@ function HomePage() {
 						</a>
 					))}
 
-					<button className={s.paginationButtonNext} onClick={nextPage}>
+					<button className={`${s.paginationLink} ${s.paginationButton}`} onClick={prevPage}>
 						<span className='visually-hidden'>Next Page</span>
+						<svg className={s.paginationIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+							<path d="M313.5 239c9.4 9.4 9.4 24.6 0 33.9l-200 200c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l183-183-183-183c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l200 200z" />
+						</svg>
 					</button>
 				</div>
 			</main>
