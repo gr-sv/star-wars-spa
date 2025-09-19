1a222ef1db8acb9081003ecd590e96f7abe067c1
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-19 22:32:23 +0300

    Add titles, styles

--- src/pages/characterPage/characterPage.tsx
+++ src/pages/characterPage/characterPage.tsx
@@ -13,7 +13,13 @@ function CharacterPage() {
 					<div className={s.cardWrapper}>
 						<div className={s.cardImage} />
 
-						<div className={s.cardInformation}></div>
+						<div className={s.cardInformation}>
+							<h2 className={s.cardTitle_main}>Character's Name</h2>
+							<h3 className={s.cardTitle}>Birth year: </h3>
+							<h3 className={s.cardTitle}>Gender: </h3>
+							<h3 className={s.cardTitle}>Height: </h3>
+							<h3 className={s.cardTitle}>Mass: </h3>
+						</div>
 					</div>
 
 				</Wrapper>
