d813aa84f1a7b941e35d7e02940e676b9635325a
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-09-23 22:23:45 +0300

    Add Search into Header, styles, correction

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import s from './header.module.scss';
 import Wrapper from '../wrapper/wrapper';
+import Search from '../search/search';
+import s from './header.module.scss';
 
 function Header() {
 	return (
 		<header className={s.header}>
-			<Wrapper>
+			<Wrapper className={s.headerWrapper}>
 				<h1 className='visually-hidden'>Star Wars</h1>
 
 				<a href='#' className={s.headerLink}>
 					<img className={s.headerLogo} src='/assets/images/logo.png' alt='star wars logo'/>
 				</a>
+
+				<Search />
 			</Wrapper>
 		</header>
 	)	
